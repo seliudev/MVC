ced5d686d1868d34f04ea9ca18bc1d30421056dd
SELİN <SELN@DESKTOP-D0QT1F4>
2024-10-13 18:51:59 +0300

    Add project files.

---
+++ BLL/DAL/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace BLL.DAL
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; } //array but we will name it collection
+        //one to many relation in database which is "one category can have many products"
+    }
+}
+
+//code first approach: we create our entities then database is created from them.
---
+++ BLL/DAL/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable 
+
+namespace BLL.DAL
+{
+    public class Product
+    {
+        //private int id;
+
+        //public void SetId()
+        //{
+        //    this.id = id;
+        //}
+
+        //public int GetId() 
+        //{ 
+        //    return this.id;
+        //}
+
+        public int Id { get; set; } // PK
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public int CategoryId { get; set; } // FK
+        public int? StockAmount { get; set; } //Nullable value type using ? 
+        public Category Category { get; set; }
+    }
+}
