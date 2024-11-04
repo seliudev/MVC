b4fc4f28da205d44bfdff9c2b2832e3961ffb508
Seliudev <Seliudev@DESKTOP-D0QT1F4>
2024-11-04 19:02:37 +0300

    update

--- BLL/Services/CategoryService.cs
+++ BLL/Services/CategoryService.cs
@@ -1,4 +1,6 @@
 ﻿using BLL.DAL;
+using BLL.Models;
+using BLL.Services.Bases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +20,33 @@ namespace BLL.Services
         {
             _db.Categories.Add(record);
             _db.SaveChanges();
+            return Success("Category is created successfully.");
         }
 
+        public Service Delete (int id)
+        {
+            //Category category = _db.Categories.Find(id);
+
+            Category category = _db.Categories.SingleOrDefault(c => c.Id == id);
+            if (category is null)
+            {
+                return Error("Category is not found.");
+            }
+            _db.Remove(category);
+            _db.SaveChanges();
+            return Success("Category is deleted successfully.");
+        }
+
+        public IQueryable<CategoryModel> Query () {
+            return _db.Categories.Select(c => new CategoryModel()
+            {
+                Record = c;
+            });
     }
+
+
+
+
 }
 
 //Dependency injection
